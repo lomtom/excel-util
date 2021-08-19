77cc6c187fdca8a286dd3e300a022470456f2d3b
longtong <ouyang@xlauncher.io>
2021-08-19 11:29:39 +0800

    add export

--- src/main/java/com/lomtom/excel/ExcelExportUtils.java
+++ src/main/java/com/lomtom/excel/ExcelExportUtils.java
@@ -4,12 +4,14 @@ import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import com.lomtom.entity.CommodityTemplate;
 import com.lomtom.entity.OssProperties;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 /**
@@ -18,17 +20,30 @@ import java.util.List;
  **/
 public class ExcelExportUtils {
 
-    public static <T> String exportExcelByOss(String rootPath,String fileName, Class<T> tClass, OssProperties ossProperties, List<T> data, ExportParams params) throws Exception {
-        return exportExcelByOss(rootPath, fileName,tClass, ossProperties.getBucket(), ossProperties.getRegionId(), ossProperties.getAk(), ossProperties.getSk(),data, params);
+    public static <T> String exportExcelToOss(String rootPath, String fileName, Class<T> tClass, OssProperties ossProperties, List<T> data, ExportParams params) throws Exception {
+        return exportExcelToOss(rootPath, fileName,tClass, ossProperties.getBucket(), ossProperties.getRegionId(), ossProperties.getAk(), ossProperties.getSk(),data, params);
     }
 
 
-    public static <T> String exportExcelByLocal(String rootPath,String fileName, Class<T> tClass, OssProperties ossProperties, List<T> data, ExportParams params) throws Exception {
-        return exportExcelByLocal(rootPath, fileName,tClass, ossProperties.getBucket(), ossProperties.getRegionId(), ossProperties.getAk(), ossProperties.getSk(),data, params);
+    public static <T> String exportExcelToLocal(String rootPath, String fileName, Class<T> tClass, List<T> data, ExportParams params) throws Exception {
+        String fullPath = rootPath + fileName;
+        Workbook sheets = ExcelExportUtil.exportExcel(params, tClass, data);
+        FileOutputStream stream = new FileOutputStream(fullPath);
+        sheets.write(stream);
+        stream.close();
+        return fullPath;
     }
 
 
-    private static <T> String exportExcelByOss(String rootPath,String fileName, Class<T> tClass, String bucket, String regionId, String accessKeyId, String accessKeySecret,List<T> data, ExportParams params) throws Exception {
+    public static <T> byte[] exportExcelToOutPutStream(Class<T> tClass, List<T> data, ExportParams params) throws Exception {
+        Workbook sheets = ExcelExportUtil.exportExcel(params, tClass, data);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        sheets.write(stream);
+        return stream.toByteArray();
+    }
+
+
+    private static <T> String exportExcelToOss(String rootPath, String fileName, Class<T> tClass, String bucket, String regionId, String accessKeyId, String accessKeySecret, List<T> data, ExportParams params) throws Exception {
         String head = "https://oss-" + regionId + ".aliyuncs.com";
         String root = "https://" + bucket + ".oss-" + regionId + ".aliyuncs.com/";
         String fullPath = rootPath + fileName;
@@ -41,14 +56,5 @@ public class ExcelExportUtils {
         bos.close();
         ossClient.shutdown();
         return root + fullPath;
-    }
-
-    private static <T> String exportExcelByLocal(String rootPath,String fileName, Class<T> tClass, String bucket, String regionId, String accessKeyId, String accessKeySecret,List<T> data, ExportParams params) throws Exception {
-        String fullPath = rootPath + fileName;
-        Workbook sheets = ExcelExportUtil.exportExcel(params, tClass, data);
-        FileOutputStream stream = new FileOutputStream(fullPath);
-        sheets.write(stream);
-        stream.close();
-        return fullPath;
     }
 }
